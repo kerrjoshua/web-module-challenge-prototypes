6885b0a5830508712f4dd2385da4f25030306113
Joshua Kerr <kerrjoshua@gmail.com>
2022-10-25 22:01:46 +0900

    Complete Task 2: car (mvp goals only)

--- index.js
+++ index.js
@@ -59,8 +59,17 @@ console.log(person1.stomach);
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
 
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+
+}
+
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
 }
 
 
